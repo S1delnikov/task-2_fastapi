14d46d6fb7a8e9431f4a842aac5d0ec6de8b7033
Misha <misha.s1d@mail.ru>
2024-03-15 20:20:43 +0300

    update requirements.txt

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
+
 # URL_DATABASE = 'postgresql://cactus:99936022MCmc@localhost:5432/fastapi_with_pg'
-URL_DATABASE = 'postgres://cactus:rH1jdjGcVGEuKQ7wSFg20dqEjn7fXcGI@dpg-cnq6s0v79t8c7396mmo0-a.frankfurt-postgres.render.com/task2'
+
+# ssl_context = ssl.create_default_context()
+# ssl_context.check_hostname = False
+# ssl_context.verify_mode = ssl.CERT_NONE
+# engine = sa.create_engine(
+#     "postgresql+pg8000://scott:tiger@192.168.0.199/test",
+#     connect_args={"ssl_context": ssl_context},
+# )
+
+URL_DATABASE = 'postgresql+psycopg2://cactus:rH1jdjGcVGEuKQ7wSFg20dqEjn7fXcGI@dpg-cnq6s0v79t8c7396mmo0-a.frankfurt-postgres.render.com/task2?sslmode=require'
 
 engine = create_engine(URL_DATABASE)
 
